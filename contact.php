7f61860473c3b8b41f144b3aa909883526d5abcd
Ravi Goswami <zneroravi@gmail.com>
2017-10-26 11:13:35 +0530

    new files

---
+++ contact.php
@@ -0,0 +1,80 @@
+<?php  include "includes/db.php"; ?>
+ <?php  include "includes/header.php"; ?>
+ 
+<?php 
+
+
+if(isset($_POST['submit'])) {
+
+$to         = "support@edwindiaz.com";
+$subject    = $_POST['subject'];
+$body       = $_POST['body'];
+    
+
+  
+
+}
+
+
+
+
+
+
+
+
+?>
+ 
+ 
+ 
+ 
+ 
+ 
+
+    <!-- Navigation -->
+    
+    <?php  include "includes/navigation.php"; ?>
+    
+    
+    
+ 
+    <!-- Page Content -->
+    <div class="container">
+    
+<section id="login">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 col-xs-offset-3">
+                <div class="form-wrap">
+                <h1>Contact</h1>
+                    <form role="form" action="" method="post" id="login-form" autocomplete="off">
+                       
+    
+                         <div class="form-group">
+                            <label for="email" class="sr-only">Email</label>
+                            <input type="email" name="email" id="email" class="form-control" placeholder="Enter your Email">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="subject" class="sr-only">Subject</label>
+                            <input type="text" name="subject" id="subject" class="form-control" placeholder="Enter your Subject">
+                        </div>
+                         <div class="form-group">
+                           
+                           <textarea class="form-control" name="body" id="body" cols="50" rows="10"></textarea>
+                        </div>
+                
+                        <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Submit">
+                    </form>
+                 
+                </div>
+            </div> <!-- /.col-xs-12 -->
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
+</section>
+
+
+        <hr>
+
+
+
+<?php include "includes/footer.php";?>
